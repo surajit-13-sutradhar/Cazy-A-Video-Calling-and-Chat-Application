f1aa9d68efd1393fae31aef3e120b616a29ef556
Surajit Sutradhar <surajit_ug_23@ee.nits.ac.in>
2025-09-24 15:04:16 +0530

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { ENV } from './config/env.js';
+import { connectDB } from './config/db.js';
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello baraxa & swikriti");
+})
+
+console.log(ENV.MONGO_URI); // for testing if env variable is working
+
+app.listen(ENV.PORT, () => {
+    console.log("Server started on port " + ENV.PORT)
+    connectDB();
+});
